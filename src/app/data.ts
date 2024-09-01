6df8e3afa5d57fbefd4a8222347f0a004341091a
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2024-09-01 14:36:37 +0530

    completed the taslk

---
+++ src/app/data.ts
@@ -0,0 +1,17 @@
+let dataArray: any = [
+    {
+        fullName: 'Ashok Gupta',
+        email: 'ashok@gmail.com',
+        password: 'Ashok@123',
+        mobileNo: '8524698745',
+        organisationName: 'Apple',
+        organisationId: 'AB1234',
+        designation: 'Developer',
+        dob: '1943-01-24',
+        city: 'Varanasi',
+        pincode: 221001
+    }
+]
+
+
+export default dataArray;
